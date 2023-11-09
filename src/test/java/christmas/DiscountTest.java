58ecf8ff38b2ebbcbf68258203b23921233fe6bc
jhon3242 <rldotnr2@gmail.com>
2023-11-09 20:41:43 +0900

    refactor : 더 다양한 테스트케이스를 활용하도록 리팩터링

--- src/test/java/christmas/DiscountTest.java
+++ src/test/java/christmas/DiscountTest.java
@@ -9,12 +9,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class DiscountTest {
+    // TODO FIxture 도입
+    private static final DecemberDate WEEKEND = new DecemberDate(1);
+    private static final DecemberDate WEEKDAY = new DecemberDate(3);
 
     @DisplayName("크리스마스 디데이 이벤트 할인 금액 계산이 문제 없다.")
     @ParameterizedTest
@@ -48,29 +50,33 @@ public class DiscountTest {
         );
     }
 
-    @DisplayName("주말에만 메 하나 당 2023원 할인이 적용된다.")
+    @DisplayName("주말에만 메인 메뉴 하나 당 2023원 할인이 적용된다.")
     @ParameterizedTest
-    @MethodSource("weekDiscountProvider")
-    void weekendDiscount(DecemberDate date, Money expected) {
+    @MethodSource("weekendDiscountProvider")
+    void weekendDiscount(Menus menus, DecemberDate date, Money expected) {
         // given
         WeekDiscount discount = new WeekDiscount();
-        Map<Menu, Integer> menuRepository = new HashMap<>();
-        menuRepository.put(Menu.T_BORN_STAKE, 1);
-        menuRepository.put(Menu.BBQ_RIBS, 1);
-        menuRepository.put(Menu.MUSHROOM_CREAM_SOUP, 1);
+
         // when
-        Menus menus = new Menus(menuRepository);
         Money actual = discount.calculateDiscountAmount(date, menus);
 
         // then
         assertThat(actual).isEqualTo(expected);
     }
 
-    static Stream<Arguments> weekDiscountProvider() {
+    static Stream<Arguments> weekendDiscountProvider() {
         return Stream.of(
-                Arguments.of(new DecemberDate(1), new Money(2023 * 2)),
-                Arguments.of(new DecemberDate(2), new Money(2023 * 2)),
-                Arguments.of(new DecemberDate(3), new Money(0)),
-                Arguments.of(new DecemberDate(4), new Money(0)));
+                Arguments.of(makeMenusByList(List.of(Menu.CHAMPAGNE, Menu.BBQ_RIBS, Menu.T_BORN_STAKE)), WEEKEND,
+                        new Money(2023 * 2)),
+                Arguments.of(makeMenusByList(List.of(Menu.CHAMPAGNE, Menu.BBQ_RIBS, Menu.T_BORN_STAKE)), WEEKDAY,
+                        new Money(0)));
+    }
+
+    private static Menus makeMenusByList(List<Menu> menus) {
+        Map<Menu, Integer> menuRepository = new HashMap<>();
+        menus.forEach(menu ->
+                menuRepository.put(menu, menuRepository.getOrDefault(menu, 0) + 1)
+        );
+        return new Menus(menuRepository);
     }
 }
