aeb96248ae440f542ea3bb90667f43fb9e1ec343
jhon3242 <rldotnr2@gmail.com>
2023-11-10 13:24:51 +0900

    test : 메뉴 입력에 대한 예외 케이스 작성

---
+++ src/test/java/christmas/MenusTest.java
@@ -0,0 +1,54 @@
+package christmas;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MenusTest {
+
+    @DisplayName("메뉴의 형식이 올바른 경우 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"타파스-1,제로콜라-1", "타파스-1", "티본스테이크-1,바비큐립-1,초코케이스-10,제로콜라-23"})
+    void menusValidFormat(String inputValue) {
+        Assertions.assertThatNoException().isThrownBy(() -> {
+            Menus.createByString(inputValue);
+        });
+    }
+
+    @DisplayName("메뉴의 형식이 잘못된 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"타파스-1,제로콜라-a", "타파스-1 제로콜라-2"})
+    void menusInvalidFormat(String inputValue) {
+        Assertions.assertThatThrownBy(() -> {
+            Menus.createByString(inputValue);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴에 포함되지 않은 메뉴가 있는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"없는메뉴-1,제로콜라-1", "없는메뉴-1,없는메뉴-2"})
+    void menusNotIncludeMenu(String inputValue) {
+        Assertions.assertThatThrownBy(() -> {
+            Menus.createByString(inputValue);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("중복된 메뉴가 있는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"타파스-1,타파스-1", "제로콜라-1,제로콜라-2"})
+    void menusDuplicateMenu(String inputValue) {
+        Assertions.assertThatThrownBy(() -> {
+            Menus.createByString(inputValue);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴의 수량이 올바르지 않는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"타파스-0,제로콜라-1", "타파스-1,제로콜라-1.2", "타파스-1,제로콜라--10"})
+    void menusInvalidCountMenu(String inputValue) {
+        Assertions.assertThatThrownBy(() -> {
+            Menus.createByString(inputValue);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
