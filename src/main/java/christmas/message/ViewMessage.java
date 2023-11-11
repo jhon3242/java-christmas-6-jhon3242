3e34eabf34bbd52c2f589e9143f90021c47f17cd
jhon3242 <rldotnr2@gmail.com>
2023-11-11 14:11:23 +0900

    refactor : 메시지 상수 별도 객체에서 관리

--- src/main/java/christmas/message/ViewMessage.java
+++ src/main/java/christmas/message/ViewMessage.java
@@ -1,10 +1,24 @@
 package christmas.message;
 
-public class ViewMessage {
+import java.text.DecimalFormat;
 
+public class ViewMessage {
+    public static final String MINUS = "-";
+    public static final String NOTHING = "없음";
 
     public static final String INPUT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String INPUT_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-
+    public static final DecimalFormat OUTPUT_FORMAT_MONEY = new DecimalFormat("###,###원");
+    public static final String OUTPUT_WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String OUTPUT_FORMAT_EVENT_PRE_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String OUTPUT_FORMAT_DISCOUNT = "%s: -%s\n";
+    public static final String OUTPUT_FORMAT_MENU = "%s - %d개\n";
+    public static final String OUTPUT_TITLE_ORDER_MENU = "<주문 메뉴>";
+    public static final String OUTPUT_TITLE_PRE_DISCOUNT_TOTAL_PRICE = "<할인 전 총주문 금액>";
+    public static final String OUTPUT_TITLE_GIFT_MENU = "<증정 메뉴>";
+    public static final String OUTPUT_TITLE_DISCOUNT = "<혜택 내역>";
+    public static final String OUTPUT_TITLE_TOTAL_DISCOUNT = "<총혜택 금액>";
+    public static final String OUTPUT_TITLE_DISCOUNTED_TOTAL_PRICE = "<할인 후 예상 결제 금액>";
+    public static final String OUTPUT_TITLE_EVENT_BADGE = "<12월 이벤트 배지>";
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,36 +1,37 @@
 package christmas.view;
 
+import static christmas.message.ViewMessage.MINUS;
+import static christmas.message.ViewMessage.NOTHING;
+import static christmas.message.ViewMessage.OUTPUT_FORMAT_DISCOUNT;
+import static christmas.message.ViewMessage.OUTPUT_FORMAT_EVENT_PRE_MESSAGE;
+import static christmas.message.ViewMessage.OUTPUT_FORMAT_MENU;
+import static christmas.message.ViewMessage.OUTPUT_FORMAT_MONEY;
+import static christmas.message.ViewMessage.OUTPUT_TITLE_DISCOUNT;
+import static christmas.message.ViewMessage.OUTPUT_TITLE_DISCOUNTED_TOTAL_PRICE;
+import static christmas.message.ViewMessage.OUTPUT_TITLE_EVENT_BADGE;
+import static christmas.message.ViewMessage.OUTPUT_TITLE_GIFT_MENU;
+import static christmas.message.ViewMessage.OUTPUT_TITLE_ORDER_MENU;
+import static christmas.message.ViewMessage.OUTPUT_TITLE_PRE_DISCOUNT_TOTAL_PRICE;
+import static christmas.message.ViewMessage.OUTPUT_TITLE_TOTAL_DISCOUNT;
+import static christmas.message.ViewMessage.OUTPUT_WELCOME_MESSAGE;
+
 import christmas.DecemberDate;
 import christmas.Menu;
 import christmas.Menus;
 import christmas.Money;
 import christmas.message.ExceptionMessage;
-import java.text.DecimalFormat;
 import java.util.Map;
 import java.util.Objects;
 
 public class OutputView {
-    private static final String OUTPUT_WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private static final DecimalFormat FORMAT_MONEY = new DecimalFormat("###,###원");
-    public static final String FORMAT_EVENT_PRE_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    public static final String FORMAT_MENU = "%s - %d개\n";
-    public static final String TITLE_ORDER_MENU = "<주문 메뉴>";
-    public static final String TITLE_PRE_DISCOUNT_TOTAL_PRICE = "<할인 전 총주문 금액>";
-    public static final String MINUS = "-";
-    public static final String TITLE_GIFT_MENU = "<증정 메뉴>";
-    public static final String NOTHING = "없음";
-    public static final String FORMAT_DISCOUNT = "%s: -%s\n";
-    public static final String TITLE_DISCOUNT = "<혜택 내역>";
-    public static final String TITLE_TOTAL_DISCOUNT = "<총혜택 금액>";
-    public static final String TITLE_DISCOUNTED_TOTAL_PRICE = "<할인 후 예상 결제 금액>";
-    public static final String TITLE_EVENT_BADGE = "<12월 이벤트 배지>";
+
 
     public static void printWelcome() {
         System.out.println(OUTPUT_WELCOME_MESSAGE);
     }
 
     public static void printEventPreMessage(DecemberDate date) {
-        System.out.printf(FORMAT_EVENT_PRE_MESSAGE, date.dateAmount());
+        System.out.printf(OUTPUT_FORMAT_EVENT_PRE_MESSAGE, date.dateAmount());
     }
 
     public static void printException(IllegalArgumentException exception) {
@@ -38,18 +39,18 @@ public class OutputView {
     }
 
     public static void printOrderMenus(Menus menus) {
-        System.out.println(TITLE_ORDER_MENU);
+        System.out.println(OUTPUT_TITLE_ORDER_MENU);
         menus.menuRepository()
                         .forEach(OutputView::printMenu);
         System.out.println();
     }
 
     private static void printMenu(Menu menu, int count) {
-        System.out.printf(FORMAT_MENU, menu.getName(), count);
+        System.out.printf(OUTPUT_FORMAT_MENU, menu.getName(), count);
     }
 
     public static void printBeforeDiscountPrice(Money price) {
-        System.out.println(TITLE_PRE_DISCOUNT_TOTAL_PRICE);
+        System.out.println(OUTPUT_TITLE_PRE_DISCOUNT_TOTAL_PRICE);
         printMoney(price, false);
         System.out.println();
     }
@@ -59,11 +60,11 @@ public class OutputView {
         if (isNotZero && isMinus) {
             System.out.print(MINUS);
         }
-        System.out.println(FORMAT_MONEY.format(money.amount()));
+        System.out.println(OUTPUT_FORMAT_MONEY.format(money.amount()));
     }
 
     public static void printGift(Menu gift) {
-        System.out.println(TITLE_GIFT_MENU);
+        System.out.println(OUTPUT_TITLE_GIFT_MENU);
         if (Objects.isNull(gift)) {
             System.out.println(NOTHING);
             System.out.println();
@@ -74,7 +75,7 @@ public class OutputView {
     }
 
     public static void printDiscountLogs(Map<String, Money> discountLog) {
-        System.out.println(TITLE_DISCOUNT);
+        System.out.println(OUTPUT_TITLE_DISCOUNT);
         if (isNoDiscount(discountLog)) {
             System.out.println(NOTHING);
             System.out.println();
@@ -85,7 +86,7 @@ public class OutputView {
     }
 
     private static void printDiscountLog(String key, Money discount) {
-        System.out.printf(FORMAT_DISCOUNT, key, FORMAT_MONEY.format(discount.amount()));
+        System.out.printf(OUTPUT_FORMAT_DISCOUNT, key, OUTPUT_FORMAT_MONEY.format(discount.amount()));
     }
 
     private static boolean isNoDiscount(Map<String, Money> discountLog) {
@@ -93,20 +94,20 @@ public class OutputView {
     }
 
     public static void printTotalDiscountMoney(Money totalDiscountMoney) {
-        System.out.println(TITLE_TOTAL_DISCOUNT);
+        System.out.println(OUTPUT_TITLE_TOTAL_DISCOUNT);
         printMoney(totalDiscountMoney, true);
         System.out.println();
     }
 
     public static void printFinalPrice(Money finalPrice) {
-        System.out.println(TITLE_DISCOUNTED_TOTAL_PRICE);
+        System.out.println(OUTPUT_TITLE_DISCOUNTED_TOTAL_PRICE);
         printMoney(finalPrice, false);
         System.out.println();
     }
 
 
     public static void printEventBadge(String eventBadge) {
-        System.out.println(TITLE_EVENT_BADGE);
+        System.out.println(OUTPUT_TITLE_EVENT_BADGE);
         System.out.println(eventBadge);
         System.out.println();
     }
